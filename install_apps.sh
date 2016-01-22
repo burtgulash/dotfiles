091341943d7ae968c4762c48c74ad56219079cd3
burtgulash <burtgulash@gmail.com>
2016-01-22 17:35:50 +0100

    Add ranger to apps

--- install_apps.sh
+++ install_apps.sh
@@ -5,6 +5,8 @@ sudo apt-get install vim
 sudo apt-get install ack-grep
 
 sudo apt-get install smplayer
+sudo apt-get install gpicview
+sudo apt-get install ranger caca-utils highlight atool poppler-utils mediainfo
 
 read -p "Install python3? (y/N)" -n 1 -r
 echo
