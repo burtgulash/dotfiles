fc7864b40227fc5c7691a78ff0a1a58e4982e562
burtgulash <burtgulas@gmail.com>
2016-02-02 12:02:35 +0100

    Revert lock back

--- scripts/blurry_i3lock.sh
+++ scripts/blurry_i3lock.sh
@@ -7,12 +7,6 @@ mogrify -scale 5% -scale 2000% ${IMG}
 #mogrify -blur 0x8 ${IMG}
 
 i3lock -i ${IMG}
-lock_pid=$$
 
-# sleep for one minute. If the lock is still locked (pid exists), then turn off
-# the screen
 sleep 60
-if ps -p ${lock_pid} > /dev/null
-then
-    xset dpms force off
-fi
+pgrep i3lock && xset dpms force off
