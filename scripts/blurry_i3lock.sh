5722bd600687758e483d20ee65e45e6ca1d53e72
burtgulash <burtgulas@gmail.com>
2016-02-02 11:58:51 +0100

    Fix i3lock pid

--- scripts/blurry_i3lock.sh
+++ scripts/blurry_i3lock.sh
@@ -2,8 +2,17 @@
 
 IMG=/tmp/screen_locked.png
 
-scrot ${IMGtmp}
+scrot ${IMG}
 mogrify -scale 5% -scale 2000% ${IMG}
 #mogrify -blur 0x8 ${IMG}
+
 i3lock -i ${IMG}
-sleep 60; pgrep i3lock && xset dpms force off
+lock_pid=$$
+
+# sleep for one minute. If the lock is still locked (pid exists), then turn off
+# the screen
+sleep 60
+if ps -p ${lock_pid} > /dev/null
+then
+    xset dpms force off
+fi
